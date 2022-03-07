3cd1e0f91c235cf2df9c892b9fe5b6c895710d06
rp46671 <ravip8527@gmail.com>
2022-03-07 22:59:36 +0530

    add some module

--- src/app/components/pages/flights-grid-left/view-ticket/view-ticket.component.ts
+++ src/app/components/pages/flights-grid-left/view-ticket/view-ticket.component.ts
@@ -10,31 +10,55 @@ import { LocationsService } from 'src/app/providers/locations.service';
 export class ViewTicketComponent implements OnInit {
   statusData: any = null;
   showingValue?: boolean;
+  refrenceNumber: any;
+  loading!: boolean;
+  disableButton!: boolean;
   constructor(
-    private _locationsService: LocationsService, private activateRoute:ActivatedRoute
+    private _locationsService: LocationsService, private activateRoute: ActivatedRoute
   ) {
     this.showingValue = false;
     this.statusData = null;
+    this.loading = true;
+    this.disableButton = true;
+
   }
   ngOnInit(): void {
     this.showingValue = false;
-    this.activateRoute.params.subscribe(parms=>{
+    this.loading = false;
+    this.activateRoute.params.subscribe(parms => {
       console.log(parms)
-      if(parms.pnr!=null && parms.pnr!='null'){
-        this.getTicket(parms)
+      if (parms.pnr == 'null') {
+
+      } else {
+        this.getTicket(parms.pnr)
       }
     })
   }
 
   getTicket(refrenceNumber: any) {
-    this.showingValue = true;
-    if (refrenceNumber.value) {
-      this._locationsService.ama_print(String(refrenceNumber.value)).subscribe((res) => {
+    this.loading = true;
+    if (refrenceNumber) {
+      this._locationsService.ama_print(String(refrenceNumber)).subscribe((res: any) => {
+       
+        if (res.airlines.status == "0" && res.airlines.error == "Error") {
+          this.showingValue = false;
 
-        this.statusData = res;
-
+          confirm("Please Enter right Pnr No");
+        } else {
+          this.showingValue = true;
+          this.statusData = res;
+        }
+        this.loading = false;
         console.log("this.showingValue", this.showingValue);
       });
+    }
+    
+  }
+  myButtonDisable(refrenceNumber: any) {
+    if (refrenceNumber.length==6) {
+      this.disableButton = false;
+    } else {
+      this.disableButton = true;
     }
   }
 }
--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HelperService } from '../../helper/helper.service';
 import { AuthService } from 'src/app/providers/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,25 @@ import { AuthService } from 'src/app/providers/auth.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent extends HelperService {
+  localUserIds: any;
+  localUsertype: any;
  constructor(
-  public authService: AuthService){
+  public authService: AuthService,
+  private route:Router){
    super();
+
+   var localUserId: any = window.sessionStorage.getItem('fight-user');
+   localUserId = JSON.parse(localUserId);
+   this.localUserIds = localUserId?.detail.id;
+   this.localUsertype=localUserId?.detail.type;
+   console.log(" this.localUsertype", this.localUsertype)
  }
+
   onLogout() {  
    this.authService.signout();
   }
+myAgencyBoking(){
+  this.route.navigate(['/flight-grid-left/my-list'])
+}
+  
 }
